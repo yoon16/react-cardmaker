526b59bc487e339bda1da49f9fa2a284b21d89a1
yoon16 <yunn1209@gmail.com>
2021-03-29 14:46:45 +0900

    edit preview

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -4,12 +4,12 @@ import styles from "./preview.module.css";
 
 const Preview = ({ cards }) => (
   <section className={styles.preview}>
+    <h1 className={styles.title}>Card Preview</h1>
     <ul className={styles.cards}>
-      {cards.map((card) => (
-        <Card card={card} />
+      {Object.keys(cards).map((key) => (
+        <Card key={key} card={cards[key]} />
       ))}
     </ul>
-    <h1 className={styles.title}>Card Preview</h1>
   </section>
 );
 
